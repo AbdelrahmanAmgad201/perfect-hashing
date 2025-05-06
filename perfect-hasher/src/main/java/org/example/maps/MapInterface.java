fa4e1822a903d6a5b4fc22ef8c0d81e89002b166
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2025-05-06 07:22:35 +0300

    clean

--- perfect-hasher/src/main/java/org/example/maps/MapInterface.java
+++
@@ -1,7 +0,0 @@
-package org.example.maps;
-
-public interface MapInterface {
-    public boolean insert(String key);
-    public boolean delete(String key);
-    public boolean contains(String key);
-}
